870bd8ddebc5528366f0c59f44aacba7637d879b
yusei <yusei.9933@gmail.com>
2019-05-10 23:21:03 +0900

    created detail page

--- diary/urls.py
+++ diary/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('list/', views.List.as_view(), name='list'),
+    path('<int:pk>/', views.Detail.as_view(), name='detail'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from . import models
 
 
 class Index(TemplateView):
     template_name = 'diary/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class List(ListView):
+    template_name = 'diary/list.html'
+    model = models.Post
+    
+
+class Detail(DetailView):
+    tamlate_name = 'diary/post_detail.html'
+    model = models.Post
+    
