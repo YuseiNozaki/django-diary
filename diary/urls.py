51c20cf97a7c627959accf044727bfca7131b891
yusei <yusei.9933@gmail.com>
2019-05-11 14:11:06 +0900

    change redirect destination after update

--- diary/urls.py
+++ diary/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create/', views.Create.as_view(), name='create'),
     path('list/', views.List.as_view(), name='list'),
     path('<int:pk>/', views.Detail.as_view(), name='detail'),
+    path('<int:pk>/update/', views.Update.as_view(), name='update'),
     path('<int:pk>/delete/', views.Delete.as_view(), name='delete'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView, DetailView, DeleteView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, DeleteView, UpdateView
 from django.contrib import messages
+from django.urls import reverse_lazy
 from . import models
 
 
@@ -13,7 +14,7 @@ class Index(TemplateView):
 
 
 class Create(CreateView):
-    template_name = 'diary/create.html'
+    template_name = 'diary/form.html'
     model = models.Post
     fields = ('title', 'text')
     success_url = '/diary/list/'
@@ -36,8 +37,26 @@ class Detail(DetailView):
     template_name = 'diary/detail.html'
     model = models.Post
 
+
+class Update(UpdateView):
+    template_name = 'diary/form.html'
+    model = models.Post
+    fields = ('title', 'text')
+
+    def get_success_url(self):
+        return reverse_lazy('detail', kwargs={'pk':self.kwargs['pk']})
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Notice : updated post')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.warning(self.request, 'Notice : could not updated post')
+        return super().form_invalid(form)
+
+
+
 class Delete(DeleteView):
     template_name = 'diary/delete.html'
     model = models.Post
     success_url = '/diary/list/'
-    
